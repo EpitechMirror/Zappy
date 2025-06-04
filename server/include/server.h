48405e482bb808928db161239726b1c35eef182c
Sara28himmich <sara.himmich@epitech.eu>
2025-06-05 00:21:54 +0200

    [~] server code architecture

---
+++ server/include/server.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** server
+*/
+
+#ifndef SERVER_H_
+#define SERVER_H_
+#include "flag.h"
+
+#include <asm-generic/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+int init_server(server_config_t *conf);
+
+#endif /* !SERVER_H_ */
--- server/src/main.c
+++ server/src/main.c
@@ -5,19 +5,8 @@
 ** main
 */
 
-#include <asm-generic/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <string.h>
 #include "flag.h"
+#include "server.h"
 
 void print_help(void)
 {
@@ -30,57 +19,6 @@ void print_help(void)
     printf("  -n name1 name2 ... is the name of the teams\n");
     printf("  -c clientsNb   is the number of authorized clients per team\n");
     printf("  -f freq        is the reciprocal of time unit for execution of actions\n");
-}
-
-int init_server(server_config_t *conf)
-{
-    int server_fd;
-    int new_socket;
-    ssize_t valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    socklen_t arrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char hello[] = "hello world!";
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket failed");
-        return 84;
-    }
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt");
-        close(server_fd);
-        return 84;
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(conf->port);
-    printf("Server started on port %d\n", conf->port);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        close(server_fd);
-        return 84;
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        close(server_fd);
-        return 84;
-    }
-    new_socket = accept(server_fd, (struct sockaddr *)&address, &arrlen);
-    if (new_socket < 0) {
-        perror("accept");
-        close(server_fd);
-        return 84;
-    }
-    valread = read(new_socket, buffer, 1023);
-    buffer[valread > 0 ? valread : 0] = '\0';
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    close(new_socket);
-    close(server_fd);
-    return 0;
 }
 
 int main(int argc, char **argv)
