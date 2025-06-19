1499ead2e4f25261d5375128f9cc55fed0c7b1ef
joker4hire <zbzbb114@gmail.com>
2025-06-19 16:49:22 +0200

    [+] Added ppo still needs oreintation and need for inventory to make plv

--- server/src/functional/handle_player.c
+++ server/src/functional/handle_player.c
@@ -27,20 +27,37 @@ void send_team_names(int fd, server_config_t *conf)
 {
     for (int i = 0; conf->team_names[i] != NULL; i++) {
         printf("Debug sending the message %s\n", conf->team_names[i]);
+        send(fd, "tna ", 4, 0);
         send(fd, conf->team_names[i], strlen(conf->team_names[i]), 0);
+        send(fd, "\n", 1, 0);
     }
+}
+
+void find_player_position(int fd, char *player_name, client_t *client)
+{
+    client_t *current = client;
+
+    while (current) {
+        if (strcmp(current->team_name, player_name) == 0) {
+            char response[64];
+            snprintf(response, sizeof(response), "ppo %d %d\n", current->x, current->y);
+            send(fd, response, strlen(response), 0);
+            return;
+        }
+        current = current->next;
+    }
+    send(fd, "sbp\n", 4, 0);
 }
 
 int respond_to_server_fd(int fd, server_config_t *conf, char *client_message, client_t *client)
 {
     int x, y;
+    char PlayerName[128];
 
-    if (strncmp(client_message, "msz", 3) == 0) {
+    if (strncmp(client_message, "msz", 3) == 0)
         send_map_size(fd, conf, client_message);
-    }
-    if (strncmp(client_message, "mct", 3) == 0) {
+    if (strncmp(client_message, "mct", 3) == 0)
         send_whole_map(fd, conf);
-    }
     if (strncmp(client_message, "bct", 3) == 0) {
         if (sscanf(client_message, "bct %d %d", &x, &y) == 2) {
             send_tile_content(fd , x, y, *conf->map);
@@ -48,9 +65,14 @@ int respond_to_server_fd(int fd, server_config_t *conf, char *client_message, cl
             send(fd, "sbp\n", 4, 0);
         }
     }
-    if (strncmp(client_message, "tna", 3) == 0) {
+    if (strncmp(client_message, "tna", 3) == 0)
         send_team_names(fd, conf);
+    if (strncmp(client_message, "ppo", 3) == 0) {
+        if (sscanf(client_message, "ppo %s", PlayerName) == 1) {
+            find_player_position(fd, PlayerName, client);
+        } else {
+            send(fd, "sbp\n", 4, 0);
+        }
     }
-
     return 0;
 }
