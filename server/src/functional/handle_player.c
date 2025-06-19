1f72816fb1acd95d9823c9bd08f10d9d892157a5
joker4hire <zbzbb114@gmail.com>
2025-06-19 15:32:42 +0200

    [+] added tna and fixed the string comparingù

--- server/src/functional/handle_player.c
+++ server/src/functional/handle_player.c
@@ -5,9 +5,11 @@
 ** handle_client_data
 */
 
+#include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include "Client_Info.h"
+#include "flag.h"
 #include "server.h"
 
 int send_map_size(int fd, server_config_t *conf, char *client_message)
@@ -21,22 +23,33 @@ int send_map_size(int fd, server_config_t *conf, char *client_message)
     return 0;
 }
 
+void send_team_names(int fd, server_config_t *conf)
+{
+    for (int i = 0; conf->team_names[i] != NULL; i++) {
+        printf("Debug sending the message %s\n", conf->team_names[i]);
+        send(fd, conf->team_names[i], strlen(conf->team_names[i]), 0);
+    }
+}
+
 int respond_to_server_fd(int fd, server_config_t *conf, char *client_message, client_t *client)
 {
     int x, y;
 
-    if (strcmp(client_message, "msz")){
+    if (strncmp(client_message, "msz", 3) == 0) {
         send_map_size(fd, conf, client_message);
     }
-    if (strcmp(client_message, "mct") == 0){
+    if (strncmp(client_message, "mct", 3) == 0) {
         send_whole_map(fd, conf);
     }
     if (strncmp(client_message, "bct", 3) == 0) {
         if (sscanf(client_message, "bct %d %d", &x, &y) == 2) {
             send_tile_content(fd , x, y, *conf->map);
         } else {
-            send(fd, "sbp\n", 4, 0); // bad parameter
+            send(fd, "sbp\n", 4, 0);
         }
+    }
+    if (strncmp(client_message, "tna", 3) == 0) {
+        send_team_names(fd, conf);
     }
 
     return 0;
