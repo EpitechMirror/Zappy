0a9454d14ef882b8dd8cc003d1dc9fe66cc44d4c
joker4hire <zbzbb114@gmail.com>
2025-06-19 15:12:16 +0200

    [+] added mct and bct but send_whole_map funtion is to be remade

--- server/src/functional/handle_player.c
+++ server/src/functional/handle_player.c
@@ -23,7 +23,21 @@ int send_map_size(int fd, server_config_t *conf, char *client_message)
 
 int respond_to_server_fd(int fd, server_config_t *conf, char *client_message, client_t *client)
 {
+    int x, y;
+
     if (strcmp(client_message, "msz")){
         send_map_size(fd, conf, client_message);
     }
+    if (strcmp(client_message, "mct") == 0){
+        send_whole_map(fd, conf);
+    }
+    if (strncmp(client_message, "bct", 3) == 0) {
+        if (sscanf(client_message, "bct %d %d", &x, &y) == 2) {
+            send_tile_content(fd , x, y, *conf->map);
+        } else {
+            send(fd, "sbp\n", 4, 0); // bad parameter
+        }
+    }
+
+    return 0;
 }
